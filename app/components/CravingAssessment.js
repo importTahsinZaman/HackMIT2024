7ba1be8207d06fb81f67b3f63620a37acf83e084
importTahsinZaman <tahsinz21366@gmail.com>
2024-09-15 10:23:00 -0400

    craving assessment update

--- app/components/CravingAssessment.js
+++ app/components/CravingAssessment.js
@@ -6,7 +6,9 @@ export default function CravingAssessment({ data }) {
     <View className="p-2 bg-[#648aff] shadow rounded-2xl h-[47%] w-[92%] flex flex-row items-center justify-evenly">
       <View className="flex items-center w-[45%]">
         <Text className="text-3xl font-semibold text-white">Craving:</Text>
-        <Text className="text-3xl font-semibold text-white">{data}%</Text>
+        <Text className="text-3xl font-semibold text-white">
+          {data > 0 ? data : 0}%
+        </Text>
       </View>
       <View className="w-[45%] text-center items-center justify-center">
         <View className="w-30%">
